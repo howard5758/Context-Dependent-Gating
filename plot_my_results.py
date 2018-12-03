c7e7b32714ef761cd35915950370fdfa3f073c66
zhuokaizhao <zhuokai@uchicago.edu>
2018-12-03 14:44:27 -0600

    rewrote code in plot_my_results.py, also new results

--- plot_my_results.py
+++ plot_my_results.py
@@ -19,6 +19,36 @@ def get_accuracy(data_dir, prefix, color = [0,0,1], split = 1, description = [],
     return task_accuracy[0]
 
 
+# helper function that actually makes the plot
+def make_plots(withCIFAR, accuracies, all_labels, outputPath, graph_name, graph_title, showGraph):
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+
+    # CIFAR only has 50 tasks
+    if withCIFAR:
+        for i in range(0, 2):
+            ax.plot(accuracies[i, :], label=i)
+        ax.plot(accuracies[2, 0:50], label=2)
+    else:
+        for i in range(0, accuracies.size):
+            ax.plot(accuracies[i, :], label=i)
+
+    # for now 7 seven basic colors are provided
+    colormap = ['blue', 'green', 'red', 'cyan', 'magenta', 'yellow', 'black']
+    
+    for i, j in enumerate(ax.lines):
+        j.set_color(colormap[i])
+        j.set_label(all_labels[i])
+
+    ax1.legend(loc='lower left', prop={'size': 6})
+    ax1.set_title(graph_title)
+    plt.xlabel('Number of tasks')
+    plt.ylabel('Accuracy')
+    plt.savefig(outputPath + graph_name, format='png')
+    if showGraph:
+        plt.show()
+
+
 
 def plot_mnist():
 
@@ -61,159 +91,178 @@ def plot_mnist():
     plt.show()
 
 
-def plot_fashion_mnist():
-
-    print('Ploting fashion mnist results')
-    savedir_fashion = 'savedir/fashion_mnist/'
-    savedir_mnist = 'savedir/mnist/'
-    savedir_cifar = 'savedir/cifar/'
-
-    png_OutPath = '/Users/zhuokaizhao/Desktop/UChicago/Autumn2018/CMSC_35200_Deep_Learning_System/Project/Plots/'
-
-    all_accuracies = np.zeros((4, 100))
-
-    fashion_mnist = all_accuracies[0, :] = get_accuracy(savedir_fashion, 'fashion_mnist_SI_XdG')
-    mnist = all_accuracies[1, :] = get_accuracy(savedir_mnist, 'mnist_SI_XdG')
-    cifar = all_accuracies[2, 0:50] = get_accuracy(savedir_cifar, 'cifar_SI_XdG_50')
-
-    all_labels = ['Fashion MNIST', 'MNIST', 'CIFAR']  
-
-    fig1 = plt.figure()
-    ax1 = fig1.add_subplot(111)
-
-    # all plotting first 50 tasks
-    for i in range(0, 3):
-    	ax1.plot(all_accuracies[i, 0:50], label=i)
-        	
-
-    # CIFAR only has 20 tasks
-    #ax1.plot(all_accuracies[2, 0:20], label=2)
-
-    colormap = ['b', 'g', 'r']
-    for i, j in enumerate(ax1.lines):
-        j.set_color(colormap[i])
-        j.set_label(all_labels[i])
-
-    ax1.legend(loc=1)
-    ax1.set_title('Accuracies of SI-XdG combinations on Fashion-MNIST, MNIST, CIFAR dataset')
-    plt.xlabel('Number of tasks')
-    plt.ylabel('Accuracy')
-    plt.savefig(png_OutPath + 'fashion_mnist_results.png', format='png')
-    plt.show()
-
-
 def plot_mix(type):
+    savedir_mnist = 'savedir/mnist/'
+    savedir_fashion = 'savedir/fashion_mnist/'
+    savedir_cifar = 'savedir/cifar/'
+#********************************************************************************************************************************************
+#*******************************                                                                      ***************************************
+#******************************* General Graph 1 - Comparisons between MNIST, Fashion-MNIST and CIFAR ***************************************
+#*******************************                                                                      ***************************************
+#********************************************************************************************************************************************
+    if type == 'General':
+        print('Ploting MNIST, Fashion-MNIST and CIFAR Training results')
+        general_png_OutPath = '/Users/zhuokaizhao/Desktop/UChicago/Autumn2018/CMSC_35200_Deep_Learning_System/Project/Plots/'
 
-    if type == 'MixTraining':
-        
+        general_accuracies = np.zeros((3, 100))
+        mnist = general_accuracies[0, :] = get_accuracy(savedir_mnist, 'mnist_SI_XdG')
+        fashion_mnist = general_accuracies[1, :] = get_accuracy(savedir_fashion, 'fashion_mnist_SI_XdG')
+        cifar = general_accuracies[2, 0:50] = get_accuracy(savedir_cifar, 'cifar_SI_XdG_50')
+
+        general_labels = ['MNIST',
+                         'Fashion-MNIST',
+                         'CIFAR']
+
+        CIFARinGeneral = True
+        general_name = 'Fashion-MNIST.png'
+        general_title = 'Accuracies over 100 tasks with MNIST, Fashion-MNIST and CIFAR datasets'
+        showGeneralGraph = False
+
+        make_plots(CIFARinGeneral, general_accuracies, general_labels, general_png_OutPath, general_name, general_title, showGeneralGraph)
+
+
+#********************************************************************************************************************************************
+#********************************************************                     ***************************************************************
+#******************************************************** Mix Training Graphs ***************************************************************
+#********************************************************                     ***************************************************************
+#********************************************************************************************************************************************
+    elif type == 'MixTraining':
         print('Ploting MNIST and Fashion-MNIST MixTraining results')
-        
-        savedir_mnist = 'savedir/mnist/'
-        savedir_fashion = 'savedir/fashion_mnist/'
         savedir_mix = 'savedir/mix/' + type + '/'
-        #savedir_cifar = 'savedir/cifar/'
+        mix_png_OutPath = '/Users/zhuokaizhao/Desktop/UChicago/Autumn2018/CMSC_35200_Deep_Learning_System/Project/Plots/MixTraining/'
 
-        png_OutPath = '/Users/zhuokaizhao/Desktop/UChicago/Autumn2018/CMSC_35200_Deep_Learning_System/Project/Plots/MixTraining/'
+        #********************************************** Graph 1 - the influence of mixtraining **********************************************
+        mix1_accuracies = np.zeros((20, 100))
 
-        all_accuracies = np.zeros((20, 100))
+        mnist = mix1_accuracies[0, :] = get_accuracy(savedir_mnist, 'mnist_SI_XdG')
+        fashion_mnist = mix1_accuracies[1, :] = get_accuracy(savedir_fashion, 'fashion_mnist_SI_XdG')
+        mix_10_100_0 = mix1_accuracies[2, :] = get_accuracy(savedir_mix, 'mix_SI_XdG_10_100_0')
+        mix_10_100_50 = mix1_accuracies[3, :] = get_accuracy(savedir_mix, 'mix_SI_XdG_10_100_50')
+        # We need to a couple more experiments in this category
+        
+        mix1_labels = ['Pure MNIST', 
+                      'Pure Fashion MNIST', 
+                      'First 10 tasks trained with 100% MNIST, 0% Fashion-MNIST, later 90 tasks trained with 0% MNIST, 100% Fashion-MNIST', 
+                      'First 10 tasks trained with 100% MNIST, 0% Fashion-MNIST, later 90 tasks trained with 50% MNIST, 50% Fashion-MNIST']
 
-        # 7 results in total
-        mnist = all_accuracies[0, :] = get_accuracy(savedir_mnist, 'mnist_SI_XdG')
-        fashion_mnist = all_accuracies[1, :] = get_accuracy(savedir_fashion, 'fashion_mnist_SI_XdG')
-        mix_10_100_0 = all_accuracies[2, :] = get_accuracy(savedir_mix, 'mix_SI_XdG_10_100_0')
-        mix_10_100_50 = all_accuracies[3, :] = get_accuracy(savedir_mix, 'mix_SI_XdG_10_100_50')
-        mix_10_80_20 = all_accuracies[4, :] = get_accuracy(savedir_mix, 'mix_SI_XdG_10_80_20')
-        mix_10_60_40 = all_accuracies[5, :] = get_accuracy(savedir_mix, 'mix_SI_XdG_10_60_40')
-        mix_10_40_60 = all_accuracies[6, :] = get_accuracy(savedir_mix, 'mix_SI_XdG_10_40_60')
-        #cifar = all_accuracies[3, 0:50] = get_accuracy(savedir_cifar, 'cifar_SI_XdG_50')
+        CIFAR_Mix1 = False
+        mix1_name = 'MixTraining_1.png'
+        mix1_title = 'Accuracies over 100 tasks with both mixed and unmixed datasets'
+        showMixGraph1 = False
 
-        all_labels = ['MNIST', 
-                      'Fashion MNIST', 
-                      '10 100% MNIST, 0% Fashion-MNIST + 90 0% MNIST, 90% Fashion-MNIST', 
-                      '10 100% MNIST, 0% Fashion-MNIST + 90 50% MNIST, 50% Fashion-MNIST',
-                      '10 80% MNIST, 20% Fashion-MNIST + 90 20% MNIST, 80% Fashion-MNIST', 
-                      '10 60% MNIST, 40% Fashion-MNIST + 90 40% MNIST, 60% Fashion-MNIST',
-                      '10 40% MNIST, 60% Fashion-MNIST + 90 60% MNIST, 40% Fashion-MNIST',]  
+        make_plots(CIFAR_Mix1, mix1_accuracies, mix1_labels, mix_png_OutPath, mix1_name, mix1_title, showMixGraph1)
 
-        fig1 = plt.figure()
-        ax1 = fig1.add_subplot(111)
+        #***************************** Graph 2 - comparisons between mix data in all training tasks ***************************************
 
-        # all plotting first 50 tasks
-        for i in range(0, 7):
-            ax1.plot(all_accuracies[i, :], label=i)
-                
-        # CIFAR only has 50 tasks
-        #ax1.plot(all_accuracies[3, 0:50], label=3)
+        mix2_accuracies = np.zeros((20, 100))
 
-        colormap = ['blue', 'green', 'red', 'cyan', 'magenta', 'yellow', 'black']
-        for i, j in enumerate(ax1.lines):
-            j.set_color(colormap[i])
-            j.set_label(all_labels[i])
+        # 6 results in total
+        mnist = mix2_accuracies[0, :] = get_accuracy(savedir_mnist, 'mnist_SI_XdG')
+        fashion_mnist = mix2_accuracies[1, :] = get_accuracy(savedir_fashion, 'fashion_mnist_SI_XdG')
+        mix_10_100_0 = mix2_accuracies[2, :] = get_accuracy(savedir_mix, 'mix_SI_XdG_10_100_0')
+        mix_10_80_20 = mix2_accuracies[3, :] = get_accuracy(savedir_mix, 'mix_SI_XdG_10_80_20')
+        mix_10_60_40 = mix2_accuracies[4, :] = get_accuracy(savedir_mix, 'mix_SI_XdG_10_60_40')
+        mix_10_40_60 = mix2_accuracies[5, :] = get_accuracy(savedir_mix, 'mix_SI_XdG_10_40_60')
+        # We need to run more experiments here
 
-        ax1.legend(loc='lower left', prop={'size': 6})
-        ax1.set_title('Accuracies of SI-XdG on mix of MNIST, Fashion-MNIST dataset')
-        plt.xlabel('Number of tasks')
-        plt.ylabel('Accuracy')
-        plt.savefig(png_OutPath + 'MixTraing.png', format='png')
-        plt.show()
+        all_labels = ['Pure MNIST', 
+                      'Pure Fashion MNIST', 
+                      'First 10 tasks trained with 100% MNIST, 0% Fashion-MNIST, later 90 tasks trained with 0% MNIST, 100% Fashion-MNIST', 
+                      'First 10 tasks trained with 80% MNIST, 20% Fashion-MNIST, later 90 tasks trained with 20% MNIST, 80% Fashion-MNIST',
+                      'First 10 tasks trained with 60% MNIST, 40% Fashion-MNIST, later 90 tasks trained with 40% MNIST, 60% Fashion-MNIST' 
+                      'First 10 tasks trained with 40% MNIST, 60% Fashion-MNIST, later 90 tasks trained with 60% MNIST, 40% Fashion-MNIST'] 
 
+        CIFAR_Mix2 = False
+        mix2_name = 'MixTraining_2.png'
+        mix2_title = 'Accuracies over 100 tasks with different kinds of mixed datasets'
+        showMixGraph2 = False
+
+        make_plots(CIFAR_Mix2, mix2_accuracies, mix2_labels, mix_png_OutPath, mix2_name, mix2_title, showMixGraph2) 
+
+        
+
+#********************************************************************************************************************************************
+#***************************************************                        *****************************************************************
+#*************************************************** Review Training Graphs *****************************************************************
+#***************************************************                        *****************************************************************
+#********************************************************************************************************************************************
     elif type == 'ReviewTraining':
         print('Ploting MNIST and Fashion-MNIST ReviewTraining results')
-        
-        savedir_mnist = 'savedir/mnist/'
-        savedir_fashion = 'savedir/fashion_mnist/'
         savedir_mix = 'savedir/mix/' + type + '/'
-        #savedir_cifar = 'savedir/cifar/'
+        review_png_OutPath = '/Users/zhuokaizhao/Desktop/UChicago/Autumn2018/CMSC_35200_Deep_Learning_System/Project/Plots/ReviewTraining/'
 
-        png_OutPath = '/Users/zhuokaizhao/Desktop/UChicago/Autumn2018/CMSC_35200_Deep_Learning_System/Project/Plots/ReviewTraining/'
+        #***************************** Graph 1 - the relationships between different review frequence ***************************************
+        review1_accuracies = np.zeros((7, 100))
+        
+        mnist = review1_accuracies[0, :] = get_accuracy(savedir_mnist, 'mnist_SI_XdG')
+        fashion_mnist = review1_accuracies[1, :] = get_accuracy(savedir_fashion, 'fashion_mnist_SI_XdG')
+        review_10_0 = review1_accuracies[1, :] = get_accuracy(savedir_mix, 'review_SI_XdG_10_0')
+        review_10_2 = review1_accuracies[2, :] = get_accuracy(savedir_mix, 'review_SI_XdG_10_2')
+        review_10_5 = review1_accuracies[5, :] = get_accuracy(savedir_mix, 'review_SI_XdG_10_5')
+        review_10_10 = review1_accuracies[6, :] = get_accuracy(savedir_mix, 'review_SI_XdG_10_10') 
+        review_10_20 = review1_accuracies[7, :] = get_accuracy(savedir_mix, 'review_SI_XdG_10_20')
 
-        all_accuracies = np.zeros((20, 100))
+        review1_labels = ['SI+XdG - Pure MNIST',
+                          'SI+XdG - Pure Fashion-MNIST',
+                          'SI+XdG - First 10 task train with MNIST, later train with Fashion-MNIST, no review',
+                          'SI+XdG - First 10 task train with MNIST, later train with Fashion-MNIST, review MNIST every 2 tasks',
+                          'SI+XdG - First 10 task train with MNIST, later train with Fashion-MNIST, review MNIST every 5 tasks',
+                          'SI+XdG - First 10 task train with MNIST, later train with Fashion-MNIST, review MNIST every 10 tasks',
+                          'SI+XdG - First 10 task train with MNIST, later train with Fashion-MNIST, review MNIST every 20 tasks',]
 
-        # 7 results in total
-        mnist = all_accuracies[0, :] = get_accuracy(savedir_mnist, 'mnist_SI_XdG')
-        #fashion_mnist = all_accuracies[1, :] = get_accuracy(savedir_fashion, 'fashion_mnist_SI_XdG')
-        review_10_0 = all_accuracies[1, :] = get_accuracy(savedir_mix, 'review_SI_XdG_10_0')
-        review_10_2 = all_accuracies[2, :] = get_accuracy(savedir_mix, 'review_SI_XdG_10_2')
-        review_10_2_EWC = all_accuracies[3, :] = get_accuracy(savedir_mix, 'review_EWC_XdG_10_2')
-        #review_20_2 = all_accuracies[4, :] = get_accuracy(savedir_mix, 'review_SI_XdG_20_2')
-        #review_10_5 = all_accuracies[5, :] = get_accuracy(savedir_mix, 'review_SI_XdG_10_5')
-        #review_10_10 = all_accuracies[6, :] = get_accuracy(savedir_mix, 'review_SI_XdG_10_10') 
-        #review_10_20 = all_accuracies[7, :] = get_accuracy(savedir_mix, 'review_SI_XdG_10_20') 
-        #cifar = all_accuracies[8, 0:50] = get_accuracy(savedir_cifar, 'cifar_SI_XdG_50')
+        CIFAR_Review1 = False
+        review1_name = 'ReviewTraining_1.png'
+        review1_title = 'Accuracies over 100 tasks with different review frequencies'
+        showReviewGraph1 = False
 
-        all_labels = ['MNIST', 
-                      #'Fashion MNIST', 
-                      'SI+XdG - No review',
-                      'SI+XdG - Review every 2 tasks after first 20 tasks',
-                      'EWC+XdG - Review every 2 tasks after first 20 tasks',
-                      'SI+XdG - Review every 2 tasks after first 10 tasks',
-                      'SI+XdG - Review every 5 tasks after first 10 tasks',
-                      'SI+XdG - Review every 10 tasks after first 10 tasks',
-                      'SI+XdG - Review every 20 tasks after first 10 tasks']  
+        make_plots(CIFAR_Review1, review1_accuracies, review1_labels, review_png_OutPath, review1_name, review1_title, showReviewGraph1)
 
-        fig1 = plt.figure()
-        ax1 = fig1.add_subplot(111)
 
-        for i in range(0, 4):
-            ax1.plot(all_accuracies[i, :], label=i)
-                
-        # CIFAR only has 50 tasks
-        #ax1.plot(all_accuracies[3, 0:50], label=3)
+        #***************************** Graph 2 - the relationships between different numbers of first tasks ********************************
+        review2_accuracies = np.zeros((7, 100))
+        
+        mnist = review2_accuracies[0, :] = get_accuracy(savedir_mnist, 'mnist_SI_XdG')
+        fashion_mnist = review2_accuracies[1, :] = get_accuracy(savedir_fashion, 'fashion_mnist_SI_XdG')
+        review_10_2 = review2_accuracies[2, :] = get_accuracy(savedir_mix, 'review_SI_XdG_10_2')
+        review_20_2 = review2_accuracies[3, :] = get_accuracy(savedir_mix, 'review_SI_XdG_20_2')
+        review_40_2 = review2_accuracies[4, :] = get_accuracy(savedir_mix, 'review_SI_XdG_40_2')
 
-        colormap = ['blue', 'green', 'red', 'cyan', 'magenta', 'yellow', 'black']
-        for i, j in enumerate(ax1.lines):
-            j.set_color(colormap[i])
-            j.set_label(all_labels[i])
+        review2_labels = ['SI+XdG - Pure MNIST',
+                          'SI+XdG - Pure Fashion-MNIST',
+                          'SI+XdG - First 10 task train with MNIST, later train with Fashion-MNIST, review MNIST every 2 tasks',
+                          'SI+XdG - First 20 task train with MNIST, later train with Fashion-MNIST, review MNIST every 2 tasks',
+                          'SI+XdG - First 40 task train with MNIST, later train with Fashion-MNIST, review MNIST every 2 tasks',]
 
-        ax1.legend(loc='lower left', prop={'size': 6})
-        ax1.set_title('Accuracies of SI-XdG on MNIST dataset with review')
-        plt.xlabel('Number of tasks')
-        plt.ylabel('Accuracy')
-        plt.savefig(png_OutPath + 'ReviewTraing_SI_vs_EWC.png', format='png')
-        plt.show()
+        CIFAR_Review2 = False
+        review2_name = 'ReviewTraining_3_DiffNumFirstTasks.png'
+        review2_title = 'Accuracies over 100 tasks with different initial tasks that have been trained solely with MNIST'
+        showReviewGraph2 = False
+
+        make_plots(CIFAR_Review2, review2_accuracies, review2_labels, review_png_OutPath, review2_name, review2_title, showReviewGraph2)
+        
+        
+        #***************************** Graph 3 - the comparisons between different methods (SI vs EWC) ***********************************
+        review3_accuracies = np.zeros((3, 100))
+        
+        mnist = review3_accuracies[0, :] = get_accuracy(savedir_mnist, 'mnist_SI_XdG')
+        review_10_2 = review3_accuracies[1, :] = get_accuracy(savedir_mix, 'review_SI_XdG_10_2')
+        review_10_2_EWC = review3_accuracies[2, :] = get_accuracy(savedir_mix, 'review_EWC_XdG_10_2')
+        
+        review3_labels = ['SI+XdG - Pure MNIST',
+                          'SI+XdG - First 10 task train with MNIST, later train with Fashion-MNIST, review MNIST every 2 tasks',
+                          'EWC+XdG - First 10 task train with MNIST, later train with Fashion-MNIST, review MNIST every 2 tasks']  
+
+        CIFAR_Review3 = False
+        review3_name = 'ReviewTraining_4_SIvsEWC.png'
+        review3_title = 'Accuracies over 100 tasks with different initial tasks that have been trained solely with MNIST'
+        showReviewGraph3 = False
+
+        make_plots(CIFAR_Review3, review3_accuracies, review3_labels, review_png_OutPath, review3_name, review3_title, showReviewGraph3)
+        
 
     else:
         print("Unrecognized task type, please use MixTraining or ReviewTraining")
+
 
 def plot_cifar():
 
@@ -255,7 +304,8 @@ def plot_cifar():
 print('Generating plots')
 #plot_mnist()
 #plot_cifar()
-#plot_fashion_mnist()
+plot_mix('General')
+plot_mix('MixTraining')
 plot_mix('ReviewTraining')
 
     
